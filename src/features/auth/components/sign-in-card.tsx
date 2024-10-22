72fc48a4b18afb64fa475ab40d34cb8d11a0a000
Aman Jindal <amanjindal231@gmail.com>
2024-10-22 23:25:55 +0530

    auth pages

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -10,8 +10,17 @@ import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
 import { FaGithub } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
+import { SignInFlow } from "../types";
+import { useState } from "react";
 
-const SignInCard = () => {
+interface SignInCardProps {
+  setState: (state: SignInFlow) => void;
+}
+
+const SignInCard = ({ setState }: SignInCardProps) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <Card className="w-full h-full p-8">
       <CardHeader className="px-0 pt-0">
@@ -20,25 +29,26 @@ const SignInCard = () => {
           Use your email or another service to continue
         </CardDescription>
       </CardHeader>
-      <CardContent className="space-y-5 px-0 pb-0">
+
+      <CardContent className="px-0 pb-0 space-y-5">
         <form className="space-y-2.5">
           <Input
             disabled={false}
-            value=""
-            onChange={() => {}}
-            placeholder="Email"
-            type="email"
             required
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <Input
             disabled={false}
-            value=""
-            onChange={() => {}}
-            placeholder="Password"
-            type="password"
             required
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
-          <Button type="submit" className="w-full" size={"lg"} disabled={false}>
+          <Button type="submit" className="w-full" size="lg" disabled={false}>
             Continue
           </Button>
         </form>
@@ -47,26 +57,38 @@ const SignInCard = () => {
           <Button
             disabled={false}
             onClick={() => {}}
-            variant={"outline"}
-            size={"lg"}
+            variant="outline"
+            size="lg"
             className="w-full relative"
           >
-            <FcGoogle className="size-5 absolute top-3 left-2.5" />
+            <FcGoogle
+              className="absolute top-3 left-2.5"
+              style={{ height: "1.25rem", width: "1.25rem" }}
+            />
             Continue with Google
           </Button>
           <Button
             disabled={false}
             onClick={() => {}}
-            variant={"outline"}
-            size={"lg"}
+            variant="outline"
+            size="lg"
             className="w-full relative"
           >
-            <FaGithub className="size-5 absolute top-3 left-2.5" />
-            Continue with Github
+            <FaGithub
+              className="absolute top-3 left-2.5"
+              style={{ height: "1.25rem", width: "1.25rem" }}
+            />
+            Continue with GitHub
           </Button>
         </div>
         <p className="text-xs text-muted-foreground">
-          Don&apos;t have an account ? <span className="text-sky-700 hover:underline cursor-pointer">Sign up</span>
+          Don&apos;t have an account?{" "}
+          <span
+            onClick={() => setState("signUp")}
+            className="text-sky-700 hover:underline cursor-pointer"
+          >
+            Sign up
+          </span>
         </p>
       </CardContent>
     </Card>
--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -1,5 +1,107 @@
-const SignUpCard = () => {
-  return <div>Sign Up Card</div>;
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { FaGithub } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+import { SignInFlow } from "../types";
+import { useState } from "react";
+
+interface SignUpCardProps {
+  setState: (state: SignInFlow) => void;
+}
+
+const SignUpCard = ({ setState }: SignUpCardProps) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  return (
+    <Card className="w-full h-full p-8">
+      <CardHeader className="px-0 pt-0">
+        <CardTitle>Sign up to continue</CardTitle>
+        <CardDescription>
+          Use your email or another service to continue
+        </CardDescription>
+      </CardHeader>
+
+      <CardContent className="px-0 pb-0 space-y-5">
+        <form className="space-y-2.5">
+          <Input
+            disabled={false}
+            required
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            disabled={false}
+            required
+            type="password"
+            placeholder="Confirm password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Input
+            disabled={false}
+            required
+            type="password"
+            placeholder="Password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          <Button type="submit" className="w-full" size="lg" disabled={false}>
+            Continue
+          </Button>
+        </form>
+        <Separator />
+        <div className="flex flex-col gap-y-2.5">
+          <Button
+            disabled={false}
+            onClick={() => {}}
+            variant="outline"
+            size="lg"
+            className="w-full relative"
+          >
+            <FcGoogle
+              className="absolute top-3 left-2.5"
+              style={{ height: "1.25rem", width: "1.25rem" }}
+            />
+            Continue with Google
+          </Button>
+          <Button
+            disabled={false}
+            onClick={() => {}}
+            variant="outline"
+            size="lg"
+            className="w-full relative"
+          >
+            <FaGithub
+              className="absolute top-3 left-2.5"
+              style={{ height: "1.25rem", width: "1.25rem" }}
+            />
+            Continue with GitHub
+          </Button>
+        </div>
+        <p className="text-xs text-muted-foreground">
+          Already have an account?{" "}
+          <span
+            onClick={() => setState("signIn")}
+            className="text-sky-700 hover:underline cursor-pointer"
+          >
+            Sign in
+          </span>
+        </p>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default SignUpCard;
