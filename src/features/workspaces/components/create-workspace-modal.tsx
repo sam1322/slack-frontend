828bbe3fa195635a83f62fc8bab6cafa3de7727f
saminem <sriram.pant5@gmail.com>
2024-10-30 10:26:47 +0530

    added a workspace modal

--- src/features/workspaces/components/create-workspace-modal.tsx
+++ src/features/workspaces/components/create-workspace-modal.tsx
@@ -7,19 +7,64 @@ import {
   DialogTitle,
 } from "@/components/ui/dialog";
 import { useCreateWorkspaceModal } from "../store/use-create-workspace-modal";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { DialogDescription } from "@radix-ui/react-dialog";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 export const CreateWorkspaceModal = () => {
+  const router = useRouter();
   const [open, setOpen] = useCreateWorkspaceModal();
+  const [name, setName] = useState("");
+
+  // const { mutate, isPending } = useCreateWorkspace();
+
   const handleClose = () => {
     setOpen(false);
+    setName("");
   };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // mutate(
+    //   { name },
+    //   {
+    //     onSuccess(id) {
+    //       toast.success("Workspace created");
+    //       router.push(`/workspace/${id}`);
+    //       handleClose();
+    //     },
+    //   }
+    // );
+  };
+
+  let isPending = false;
 
   return (
     <Dialog open={open} onOpenChange={handleClose}>
       <DialogContent>
         <DialogHeader>
           <DialogTitle>Add a workspace</DialogTitle>
+          <DialogDescription className="hidden"></DialogDescription>
         </DialogHeader>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <Input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            disabled={isPending}
+            required
+            autoFocus
+            minLength={3}
+            placeholder="Workspace name e.g. 'Work', 'Personal', 'Home'"
+          />
+          <div className="flex justify-end">
+            <Button type="submit" disabled={isPending}>
+              Create
+            </Button>
+          </div>
+        </form>
       </DialogContent>
     </Dialog>
   );
